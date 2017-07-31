2eaac3959a3f99f182a8001075d00b57848b3b39
Craig Lynagh <craiglynagh@gmail.com>
2017-07-31 21:21:47 +0100

    weight and height tests pass successfully

--- Bus.java
+++ Bus.java
@@ -2,11 +2,15 @@ class Bus {
 private String name;
 private String depart;
 private String terminal;
+private int weight;
+private double height;
 
-public Bus(String name, String depart, String terminal){
+public Bus(String name, String depart, String terminal, int weight, double height){
 this.name = name;
 this.depart = depart;
 this.terminal = terminal;
+this.weight = weight;
+this.height = height;
 }
 
 public String getName(){
@@ -19,6 +23,14 @@ public String getDeparture(){
 
 public String getTerminal(){
   return this.terminal;
+}
+
+public int getWeight(){
+  return this.weight;
+}
+
+public double getHeight(){
+  return this.height;
 }
 
 
--- BusTest.java
+++ BusTest.java
@@ -7,7 +7,7 @@ public class BusTest {
 
   @Before
   public void before(){
-    bus = new Bus("Henry", "Edinburgh", "Portobello");
+    bus = new Bus("Henry", "Edinburgh", "Portobello", 1000, 4.2);
 }
 
 @Test
@@ -23,6 +23,16 @@ public void hasDeparture(){
 @Test
 public void hasTerminal(){
   assertEquals("Portobello", bus.getTerminal());
+}
+
+@Test
+public void hasWeight(){
+  assertEquals(1000, bus.getWeight());
+}
+
+@Test
+public void hasHeight(){
+  assertEquals(4.2, bus.getHeight(), 0.01);
 }
 
 
